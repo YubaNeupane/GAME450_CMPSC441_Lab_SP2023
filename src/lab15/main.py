510acd4b6255b2195a733c85f748478291c9cfbb
Yuba Neupane <yuba@Yubas-MacBook-Air.local>
2023-04-18 11:36:52 -0400

    extra credit

--- src/lab15/main.py
+++ src/lab15/main.py
@@ -1,13 +1,18 @@
 
 import spacy
+import openai
+
+openai.api_key = "PUT THE API KEY HERE"
 
 # Metric:
 # Jaccard similarity coefficient measures the similarity between two sets of data, such as the sets of words in two sentences,
 # by calculating the ratio of the number of common elements to the total number of elements. In the context of sentence similarity,
 # the Jaccard coefficient can be used to compare the sets of words in two sentences to determine how similar they are. After we get get the jaccard coefficient similarity
-# Calculate the average sentence similarity score between the two texts.
+# Also using the chat gpt api to tell the similarity between the two sentences which gives me a number from 0 - 1, where i tak the avarger between the three methods to get the most accurate
+# results to determine the similarity between the two sentences
 
-sentences = [
+
+sentences = sentences = [
     ["The estimated age of the Earth is approximately 4.54 billion years. This age is determined through a variety of scientific methods, including radiometric dating of rocks and minerals, analysis of the Earth's magnetic field, and examination of the ages of meteorites and other extraterrestrial materials that are believed to have formed around the same time as the Earth.",
      "The Earth is estimated to be around 4.54 billion years old. This age has been determined through a variety of scientific methods, including radiometric dating of rocks and meteorites, studies of the Earth's magnetic field, and analysis of the formation and decay of isotopes in minerals."
      ],
@@ -15,11 +20,23 @@ sentences = [
      "A black hole is a region of space where the gravitational pull is so strong that nothing, not even light, can escape it. It is formed when a massive star runs out of fuel and collapses under the force of its own gravity, creating a point of infinite density called a singularity. The boundary around a black hole from which nothing can escape is called the event horizon. Beyond the event horizon, the gravitational pull is so strong that even light cannot escape, making it impossible to observe directly. Black holes are classified by their mass, with stellar-mass black holes having a mass between a few to tens of times that of the sun, and supermassive black holes having a mass equivalent to millions or billions of suns. Black holes are important objects in astrophysics and have been the subject of intense study for decades. They are believed to play a significant role in shaping the structure of galaxies and the evolution of the universe."
      ],
     ["The world is made up of various elements, compounds, and particles. At its most fundamental level, the world is composed of atoms, which are the basic building blocks of matter. These atoms are made up of protons, neutrons, and electrons, which are held together by powerful forces.In addition to atoms, the world is also made up of various forms of energy, such as electromagnetic radiation and the various forms of kinetic and potential energy. The world also contains various forms of matter, such as solids, liquids, gases, and plasma, which can exist in a wide range of states and conditions.Overall, the world is a complex and diverse place, made up of countless different elements, compounds, and particles, all interacting with each other in various ways to create the many different forms and phenomena we observe around us.",
-        "The world is made up of a variety of components, including matter, energy, and forces. At its most basic level, matter is composed of atoms, which are the building blocks of everything we see and experience. These atoms combine to form molecules, which make up the different types of matter that we encounter in our daily lives, such as water, air, and rocks.Energy is another key component of the world, and it takes many different forms, such as thermal energy, light energy, and kinetic energy. Energy is essential to everything we do, from powering our homes and vehicles to driving biological processes within our bodies.Forces also play a crucial role in shaping the world around us, from the gravitational forces that hold planets in orbit to the electromagnetic forces that govern the behavior of atoms and molecules. Overall, the world is a complex and interconnected system, made up of many different components that work together to create the rich and varied environment we inhabit."]
+        "The world is made up of a variety of components, including matter, energy, and forces. At its most basic level, matter is composed of atoms, which are the building blocks of everything we see and experience. These atoms combine to form molecules, which make up the different types of matter that we encounter in our daily lives, such as water, air, and rocks.Energy is another key component of the world, and it takes many different forms, such as thermal energy, light energy, and kinetic energy. Energy is essential to everything we do, from powering our homes and vehicles to driving biological processes within our bodies.Forces also play a crucial role in shaping the world around us, from the gravitational forces that hold planets in orbit to the electromagnetic forces that govern the behavior of atoms and molecules. Overall, the world is a complex and interconnected system, made up of many different components that work together to create the rich and varied environment we inhabit."],
+    ["I like bananas and pizza", "I love going to a long bike rides"]
 ]
 
 
 nlp = spacy.load("en_core_web_md")
+
+
+def getSimilarityWithChatGPT(sentence1, sentence2):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user",
+                "content": "Give me simaity score between [1,0] for these and only give me a number not explaination: ["+sentence1+","+sentence2+"]"}
+        ]
+    )
+    return float(completion.choices[0].message.content)
 
 
 def jaccard_similarity(sentence1, sentence2):
@@ -44,6 +61,7 @@ def calculate_similarity(text1, text2):
     doc2 = nlp(text2)
     sentence_scores = []
     sentence_scores.append(jaccard_similarity(text1, text2))
+    sentence_scores.append(getSimilarityWithChatGPT(text1, text2))
     for sent1, sent2 in zip(doc1.sents, doc2.sents):
         score = sent1.similarity(sent2)
         sentence_scores.append(score)
@@ -54,14 +72,14 @@ def calculate_similarity(text1, text2):
 
 
 for sen in sentences:
-    similarity_score = calculate_similarity(sen[0], sen[1])
     print(sen[0], "\n<->\n", sen[1])
 
-    print(f"Similarity score: {similarity_score:.2f}")
+    similarity_score = calculate_similarity(sen[0], sen[1])
+
+    print(f"Similarity score: {similarity_score}")
 
     if (similarity_score > 0.78):
         print("CHATGPT IS TELLING THE TRUST 😁")
     else:
         print("CHATGPT IS THE LYING 😭")
-
     print("========================================\n")
