b62bf621001c44e423d64b4df59a03f2cfb63e7c
Yuba Neupane <yuba@Yubas-MacBook-Air.local>
2023-04-24 08:32:24 -0400

    added stuff

--- src/project/GameManager.py
+++ src/project/GameManager.py
@@ -1,18 +1,19 @@
+from lab2.cities_n_routes import get_randomly_spread_cities, get_routes
 import sys
 from pathlib import Path
 import random
 
 sys.path.append(str((Path(__file__) / ".." / "..").resolve().absolute()))
-from lab2.cities_n_routes import get_randomly_spread_cities, get_routes
+
 
 class GameManager:
     def __init__(self):
-         self.citites = [];
-         self.cityNames = []
-         self.routes = []
-         self.city_locations_dict = None
-         
-    #TODO: FIX THIS SHIT!
+        self.citites = []
+        self.cityNames = []
+        self.routes = []
+        self.city_locations_dict = None
+
+    # TODO: FIX THIS SHIT!
     def generateCityNames(self, numberOfCities):
         print("Generating city names...")
         self.cityNames = [
@@ -27,16 +28,17 @@ class GameManager:
             "Baernlad",
             "Forthyr",
         ]
-    
-    def generateCityLinks(self, mapsize:tuple):
+
+    def generateCityLinks(self, mapsize: tuple):
         print("Generating city links...")
         self.cities = get_randomly_spread_cities(mapsize, len(self.cityNames))
         self.routes = get_routes(self.cities)
         random.shuffle(self.routes)
         self.routes = self.routes[:10]
-        
-        self.city_locations_dict = {name: location for name, location in zip(self.cityNames, self.cities)}
-    
-        
+
+        self.city_locations_dict = {
+            name: location for name, location in zip(self.cityNames, self.cities)}
+        print(self.city_locations_dict)
+
     def generateStory(self):
         print("Generating story...")
